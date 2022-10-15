822c4dde1dc4f019c83ba644a7f549524969f0a2
Jed Rembold <jedediah.r@gmail.com>
2022-10-15 15:04:16 -0700

    Add new zip and template files

---
+++ Prob1.sql
@@ -0,0 +1,30 @@
+/*
+Collaborators (if any):
+Time spent (in hours):
+*/
+
+
+-- A)
+-- Answer:
+-- SQL used below
+
+
+
+-- B)
+-- Answer:
+-- SQL used below
+
+
+
+-- C)
+-- Answer:
+-- SQL used below
+
+
+
+-- D)
+-- Answer: For your answer here, just upload a CSV of the necessary table back to GitHub
+-- SQL used below
+
+
+
---
+++ Prob2.sql
@@ -0,0 +1,17 @@
+/*
+Collaborators (if any):
+Time spent (in hours):
+*/
+
+
+-- A)
+-- Answer: Include your SQL below, but then just upload a PNG or JPG of your visualization back to GitHub
+-- SQL used below
+
+
+
+-- B)
+-- Question you are interested in asking?: 
+-- What did you find out?:
+-- Remember to include both your SQL below (make sure it involved GROUP BY!) AND upload a PNG or JPG of your visualization back to GitHub
+-- SQL used below
